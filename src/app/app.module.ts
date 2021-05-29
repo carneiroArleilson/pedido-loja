4401837feff23d63feb6bd68e389a52db9e02823
carneiroArleilson <carneiroarleilson@gmail.com>
2021-05-29 16:57:12 -0400

    commit inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { OrderComponent } from './components/order/order.component';
+import { ListComponent } from './components/list/list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    OrderComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
